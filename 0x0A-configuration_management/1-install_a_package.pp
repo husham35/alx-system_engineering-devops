c23b5f21d5ad0be2fdb9756a54727fa2400d6282
husham35 <husham35@gmail.com>
2024-04-19 21:45:45 +0000

    more fixes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,4 @@
 # Using Puppet, install `flask` from `pip3`.
-# Requirements:
-#     Install `flask`
-#     Version must be `2.1.0`
-
 
 package { 'flask':
 	ensure   => '2.1.0',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,4 @@
 # Using Puppet, create a manifest that kills a process named `killmenow`.
-# Requirements:
-#     Must use the `exec` Puppet resource
-#     Must use `pkill`
 
 exec { 'pkill':
 	path    => '/usr/bin/',
