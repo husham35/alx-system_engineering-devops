fa4306c2559507dd5c825d68b115d9a363b5ff78
husham35 <husham35@gmail.com>
2024-05-20 16:49:42 +0000

    more fixes, indentation and others

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,30 +2,31 @@
 """
 Python script to export data in the CSV format.
 Requirements:
-  - Records all tasks that are owned by this employee
-  - Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-  - File name must be: USER_ID.csv
+    - Records all tasks that are owned by this employee
+    - Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    - File name must be: USER_ID.csv
 """
-import sys
-import requests
 import csv
+import requests
+import sys
 
 if __name__ == '__main__':
-  url = 'https://jsonplaceholder.typicode.com/'
-  emp_id = sys.argv[1]
-  file_name = emp_id + '.csv'
+    url = 'https://jsonplaceholder.typicode.com/'
+    emp_id = sys.argv[1]
+    file_name = emp_id + '.csv'
 
-  user = requests.get(f"{url}/users/{emp_id}").json()
-  emp_name = user.get('name')
+    user = requests.get(f"{url}/users/{emp_id}").json()
+    emp_name = user.get('name')
 
-  todos = requests.get(url + "/todos", params={"userId": emp_id}).json()
+    todos = requests.get(url + "/todos", params={"userId": emp_id}).json()
 
-  rows = []
+    rows = []
 
-  for data in todos:
-    rows.append([emp_id, emp_name, data.get('completed'), data.get('title')])
-  
-  with open(file_name, "w", newline="") as csvfile:
-    writer =  csv.writer(csvfile, delimiter=',',
-                         quotechar='"', quoting=csv.QUOTE_ALL)
-    [writer.writerow(row) for row in rows]
+    for data in todos:
+        rows.append([emp_id, emp_name, data.get('completed'),
+                     data.get('title')])
+
+    with open(file_name, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        [writer.writerow(row) for row in rows]
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,31 +2,31 @@
 """
 Python script to export data in the JSON format.
 Requirements:
-  - Records all tasks that are owned by this employee
-  - File name must be: USER_ID.json
+    - Records all tasks that are owned by this employee
+    - File name must be: USER_ID.json
 """
-import sys
-import requests
 import json
+import requests
+import sys
 
 if __name__ == '__main__':
-  url = 'https://jsonplaceholder.typicode.com/'
-  emp_id = sys.argv[1]
-  file_name = emp_id + '.json'
+    url = 'https://jsonplaceholder.typicode.com/'
+    emp_id = sys.argv[1]
+    file_name = emp_id + '.json'
 
-  user = requests.get(f"{url}/users/{emp_id}").json()
+    user = requests.get(f"{url}/users/{emp_id}").json()
 
-  todos = requests.get(url + "/todos", params={"userId": emp_id}).json()
-  emp_name = user.get('name')
+    todos = requests.get(url + "/todos", params={"userId": emp_id}).json()
+    emp_name = user.get('name')
 
-  emp_dict = {}
-  data = []
+    emp_dict = {}
+    data = []
 
-  for item in todos:
-    data.append({'task': item.get('title'), 'completed': item.get('completed'),
-                 'username': emp_name})
-  
-  emp_dict[emp_id] = data
-  with open(file_name, "w", encoding='utf-8') as jsonfile:
-    json_text_file =  json.dumps(emp_dict)
-    jsonfile.write(json_text_file)
+    for item in todos:
+        data.append({'task': item.get('title'),
+                     'completed': item.get('completed'), 'username': emp_name})
+
+    emp_dict[emp_id] = data
+    with open(file_name, "w", encoding='utf-8') as jsonfile:
+        json_text_file = json.dumps(emp_dict)
+        jsonfile.write(json_text_file)
