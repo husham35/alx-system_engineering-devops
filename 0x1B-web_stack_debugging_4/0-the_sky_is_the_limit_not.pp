aff4595e1f0d44c3aaca80dd19b3dd62998bb0ed
husham35 <husham35@gmail.com>
2024-06-13 16:31:44 +0000

    corrected task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fixing our server to get zero failed requests.
 
 exec { 'increase limit on open files':
-	command => '/bin/sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"- Hn\"/" /etc/default/nginx && /etc/init.d/nginx restart',
+  command => '/bin/sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"- Hn\"/" /etc/default/nginx && /etc/init.d/nginx restart',
 }
